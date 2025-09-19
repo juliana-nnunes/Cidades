e60da805abd0315a772358978760b7838a3450d7
juliana-nnunes <josecarloslimanunes30@gmail.com>
2025-09-19 19:51:21 -0300

    atividade concluida

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// Faça um programa que exiba os nomes das cidades do litoral sul de São Paulo, um em cada linha
+Console.WriteLine("Bertioga");
+Console.WriteLine("Cubatão");
+Console.WriteLine("Guarujá");
+Console.WriteLine("Itanhaém");
+Console.WriteLine("Mongaguá");
+Console.WriteLine("Peruíbe");
+Console.WriteLine("Praia Grande");
+Console.WriteLine("Santos");
+Console.WriteLine("São Vicente");
